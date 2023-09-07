aa98de419aa36094a610fb804ecde75fc748259a
Leucisticboi <116482248+Leucisticboi@users.noreply.github.com>
2023-09-06 22:16:16 -0600

    Created starter JS and SQL files

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS staff_info;
+CREATE DATABASE staff_info;
+
+USE staff_info;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name) VALUES
+    ('Human Resources'),
+    ('Sales'),
+    ('Marketing'),
+    ('Engineering');
+
+INSERT INTO role (title, salary, department_id) VALUES
+    ('HR Manager', 70000, 1),
+    ('Sales Manager', 90000, 2),
+    ('Sales Associate', 50000, 2),
+    ('Marketing Director', 75000, 3),
+    ('Software Engineer', 80000, 4),
+    ('IT Specialist', 65000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Jane', 'Smith', 2, NULL),
+    ('Mark', 'Johnson', 3, 2),
+    ('Sarah', 'Lee', 4, NULL),
+    ('Emily', 'Brown', 7, NULL),
+    ('Michael', 'Wilson', 5, 5),
+    ('Milo', 'Miller', 6, 5);
